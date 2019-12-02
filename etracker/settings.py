a5dc58fbeb215d5ec2f0edc40366ec51a5d04ddc
Ubuntu <ubuntu@ip-172-31-93-232.ec2.internal>
2019-12-02 18:16:02 +0000

    add allowed host for Heroku

--- etracker/settings.py
+++ etracker/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['c62fdf37f3e04a009245b583d45396a2.vfs.cloud9.us-east-1.amazonaws.com']
+ALLOWED_HOSTS = ['c62fdf37f3e04a009245b583d45396a2.vfs.cloud9.us-east-1.amazonaws.com', 'issue-tracker-application.herokuapp.com']
 
 
 # Application definition
